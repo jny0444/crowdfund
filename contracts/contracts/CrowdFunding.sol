79094a2afc2f46864333db6ad10a0b3e2ce81aae
Jnyandeep <thokchom.jnyandeep@gmail.com>
2025-02-03 00:00:21 +0530

    fix: minor fixes

---
+++ contracts/contracts/CrowdFunding.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        uint256 goal;
+        uint256 deadline;
+        uint256 balance;
+    }
+
+    
+}
